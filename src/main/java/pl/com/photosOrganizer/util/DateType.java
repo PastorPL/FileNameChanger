b7d8ddd4c0297d590ddfdbd6ab7de8927318d368
MW <michal.wieckowskii@gmail.com>
2015-12-06 20:00:08 +0100

    add radiobuttons to select date

---
+++ src/main/java/pl/com/photosOrganizer/util/DateType.java
@@ -0,0 +1,5 @@
+package pl.com.photosOrganizer.util;
+
+public enum DateType {
+	CREATION_DATE, LAST_ACCESS_DATE, MODYFIED_DATE
+}
--- src/main/java/pl/com/photosOrganizer/view/MainController.java
+++ src/main/java/pl/com/photosOrganizer/view/MainController.java
@@ -15,27 +15,35 @@ import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import pl.com.photosOrganizer.MainApp;
+import pl.com.photosOrganizer.util.DateType;
 
 public class MainController {
 	@SuppressWarnings("unused")
 	private MainApp mainApp;
 	private Stage stage;
 	private File selectedDirectory;
-	
+
 	private int filesNumber, fileCounter;
+
+	private DateType dateType = DateType.CREATION_DATE;
+
+	@FXML
+	private ToggleGroup radioGroup;
 
 	@FXML
 	private ProgressBar progressBar;
-	
+
 	@FXML
 	private TextField directoryPath;
-	
-	@FXML 
-	private Button runButton; 
+
+	@FXML
+	private Button runButton;
 
 	Task<Void> task = new Task<Void>() {
 		@Override
@@ -48,8 +56,22 @@ public class MainController {
 				stream.map(Path::toFile).filter(file -> !file.isDirectory()).sorted().collect(Collectors.toList())
 						.stream().map(File::toPath).forEach((p -> {
 					try {
-						Date date = new Date(
-								Files.readAttributes(p, BasicFileAttributes.class).creationTime().toMillis());
+						Long dateTimeMilis = null;
+						switch (dateType) {
+						case CREATION_DATE:
+							dateTimeMilis = Files.readAttributes(p, BasicFileAttributes.class).creationTime()
+									.toMillis();
+							break;
+						case LAST_ACCESS_DATE:
+							dateTimeMilis = Files.readAttributes(p, BasicFileAttributes.class).lastAccessTime()
+									.toMillis();
+							break;
+						case MODYFIED_DATE:
+							dateTimeMilis = Files.readAttributes(p, BasicFileAttributes.class).lastModifiedTime()
+									.toMillis();
+							break;
+						}
+						Date date = new Date(dateTimeMilis);
 						String extendsion = p.toFile().getName().substring(p.toFile().getName().lastIndexOf("."));
 						File checker = new File(path.toAbsolutePath() + "/" + df.format(date) + extendsion);
 						int counter = 0;
@@ -63,7 +85,7 @@ public class MainController {
 						else
 							Files.move(p, p.resolveSibling(df.format(date) + "_" + counter + extendsion));
 						fileCounter++;
-						progressBar.setProgress(fileCounter/filesNumber);
+						progressBar.setProgress(fileCounter / filesNumber);
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
@@ -79,7 +101,20 @@ public class MainController {
 	 */
 	@FXML
 	private void initialize() {
-		// Initialize the person table with the two columns.
+		this.radioGroup.selectedToggleProperty().addListener((ov, t, t1) -> {
+			RadioButton chk = (RadioButton) t1.getToggleGroup().getSelectedToggle();
+			switch (chk.getText()) {
+			case "Creation Date":
+				dateType = DateType.CREATION_DATE;
+				break;
+			case "Last Access Date":
+				dateType = DateType.LAST_ACCESS_DATE;
+				break;
+			case "Last Modified Date":
+				dateType = DateType.MODYFIED_DATE;
+				break;
+			}
+		});
 	}
 
 	@FXML
@@ -87,11 +122,11 @@ public class MainController {
 		DirectoryChooser chooser = new DirectoryChooser();
 		chooser.setTitle("Choose folder with images");
 		selectedDirectory = chooser.showDialog(this.stage);
-		if(selectedDirectory!=null){
+		if (selectedDirectory != null) {
 			directoryPath.setText(selectedDirectory.getAbsolutePath());
-			filesNumber = selectedDirectory.list((dir,name)->dir.isDirectory()).length;
+			filesNumber = selectedDirectory.list((dir, name) -> dir.isDirectory()).length;
 			runButton.setDisable(false);
-		}else{
+		} else {
 			runButton.setDisable(true);
 		}
 	}
@@ -101,8 +136,6 @@ public class MainController {
 		Thread th = new Thread(task);
 		th.setDaemon(true);
 		th.start();
-		
-		System.out.println(selectedDirectory.getAbsolutePath());
 	}
 
 	public void setMainApp(MainApp mainApp) {
